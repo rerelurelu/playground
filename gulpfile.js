843b99a3e1184b70d3534ead0edaa2423e815bf1
snova <awaawapurin@gmail.com>
2021-03-12 22:24:40 +0900

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+// Plugins
+let gulp = require('gulp');
+let postcss = require('gulp-postcss');
+let autoprefixer = require('autoprefixer');
+let stylus = require('gulp-stylus');
+
+// Path
+let filePath = {src: 'src/', dist: 'dist/'};
+let fileSrc = {
+  stylus: `${filePath.src}stylus/*.styl`
+};
+
+gulp.task('css', function () {
+  let plugins = [
+      autoprefixer({
+        browsers: ['last 2 version']
+      })
+  ];
+
+  return gulp.src(`${fileSrc.stylus}`)
+        .pipe(stylus())
+        .pipe(postcss(plugins))
+        .pipe(gulp.dest(`${filePath.dist}css/`));
+});
