0c7f725d7769f714043a0dc3f114232b60960fa6
snova <awaawapurin@gmail.com>
2021-03-19 19:59:44 +0900

    fixed

--- sidebar/sidebar.js
+++ sidebar/sidebar.js
@@ -1,11 +1,11 @@
-const toggleBtn = document.querySelector('.sidebar-toggle')
-const closeBtn = document.querySelector('.close-btn')
-const sidebar = document.querySelector('.sidebar')
+const toggleBtn = document.querySelector('.sidebar-toggle');
+const closeBtn = document.querySelector('.close-btn');
+const sidebar = document.querySelector('.sidebar');
 
-toggleBtn.addEventListener('click', function () {
-  sidebar.classList.toggle('show-sidebar')
-})
+toggleBtn.addEventListener('click', () => {
+  sidebar.classList.toggle('show-sidebar');
+});
 
-closeBtn.addEventListener('click', function () {
-  sidebar.classList.remove('show-sidebar')
-})
+closeBtn.addEventListener('click', () => {
+  sidebar.classList.remove('show-sidebar');
+});
