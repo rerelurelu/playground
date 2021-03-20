7ccb9cfe7357b352d0bbd3cb27557cf80bd66107
snova <awaawapurin@gmail.com>
2021-03-20 15:14:01 +0900

    fixed

--- counter/counter.js
+++ counter/counter.js
@@ -1,41 +1,39 @@
 const colors = [
   0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-  'A', 'B', 'C', 'D', 'E', 'F'
-]
+  'A', 'B', 'C', 'D', 'E', 'F',
+];
 
-let count = 0
+let count = 0;
 
-const value = document.querySelector('#value')
-const btns = document.querySelectorAll('.btn')
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
 
-btns.forEach(function (btn) {
-  btn.addEventListener('click', function (e) {
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
+
+btns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
     /* counter */
-    const styles = e.currentTarget.classList
+    const styles = e.currentTarget.classList;
     if (styles.contains('decrease')) {
-      count--
+      count -= 1;
+    } else if (styles.contains('increase')) {
+      count += 1;
+    } else {
+      count = 0;
     }
-    else if (styles.contains('increase')) {
-      count++
-    }
-    else {
-      count = 0
-    }
-    value.textContent = count
+    value.textContent = count;
 
     /* change background */
-    let color1 = '#'
-    let color2 = '#'
+    let color1 = '#';
+    let color2 = '#';
 
-    for (let i = 0; i < 6; i++) {
-      color1 += colors[getRandomNumber()]
-      color2 += colors[getRandomNumber()]
+    for (let i = 0; i < 6; i += 1) {
+      color1 += colors[getRandomNumber()];
+      color2 += colors[getRandomNumber()];
     }
 
-    document.body.style.background = `linear-gradient(to bottom, ${color1}, ${color2})`
-  })
-})
-
-function getRandomNumber () {
-  return Math.floor(Math.random() * colors.length)
-}
+    document.body.style.background = `linear-gradient(to bottom, ${color1}, ${color2})`;
+  });
+});
