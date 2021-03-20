8f010d087fb58c2b6f521d69d567a51f3559265b
snova <awaawapurin@gmail.com>
2021-03-21 01:41:07 +0900

    updated

--- gulpfile.js
+++ gulpfile.js
@@ -2,38 +2,80 @@
 /* Plugins */
 const chalk = require('chalk');
 const {
-  src, dest, watch, lastRun,
+  src, dest, watch, lastRun, series,
 } = require('gulp');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const fibers = require('fibers');
 const sass = require('gulp-sass');
 const cssnano = require('cssnano');
+const rename = require('gulp-rename');
+const plumber = require('gulp-plumber');
+const babel = require('gulp-babel');
+const terser = require('gulp-terser');
+const browsersync = require('browser-sync');
 sass.compiler = require('sass');
 
 /* Path */
-const filePath = {
-  src: 'src/sass/*.sass',
-  dist: 'dist/css/',
+const fileSrc = {
+  sass: 'src/sass/**/*.sass',
+  js: 'src/js/**/*.js',
+  html: '**/*.html',
 };
 
-/* compile sass into css */
+const fileDist = {
+  sass: 'dist/css/',
+  map: 'map/',
+  js: 'dist/js/',
+};
+
+/* compile sass */
 const compileSass = (cb) => {
-  src(filePath.src, { sourcemaps: true, since: lastRun(compileSass) })
+  src(fileSrc.sass, { sourcemaps: true, since: lastRun(compileSass) })
+    .pipe(plumber())
     .pipe(sass({ fiber: fibers }))
     .pipe(postcss([
       autoprefixer(),
-      cssnano({
-        autoprefixer: false,
-      }),
+      cssnano({ autoprefixer: false }),
     ]))
-    .pipe(dest(filePath.dist, { sourcemaps: '.' }));
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(dest(fileDist.sass, { sourcemaps: `${fileDist.map}` }));
+  cb();
+};
+
+/* compile Javascript */
+const compileJs = (cb) => {
+  src(fileSrc.js, { sourcemaps: true, since: lastRun(compileJs) })
+    .pipe(plumber())
+    .pipe(babel({ presets: ['@babel/preset-env'] }))
+    .pipe(terser())
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(dest(fileDist.js, { sourcemaps: `${fileDist.map}` }));
+  cb();
+};
+
+/* browsersync */
+const browsersyncServer = (cb) => {
+  browsersync.init({
+    server: {
+      baseDir: '.',
+    },
+  });
+  cb();
+};
+
+const browsersyncReload = (cb) => {
+  browsersync.reload();
   cb();
 };
 
 /* Watch task */
-const watchSassFiles = () => {
-  const watcher = watch(filePath.src, compileSass);
+const watchTask = () => {
+  watch(fileSrc.html, browsersyncReload);
+
+  const watcher = watch([fileSrc.sass, fileSrc.js],
+    series(compileSass, compileJs, browsersyncReload));
+
   const messageLog = chalk.italic.bold.hex('#7cc7e8');
 
   watcher.on('change', (path) => {
@@ -41,5 +83,12 @@ const watchSassFiles = () => {
   });
 };
 
-exports.sassw = watchSassFiles;
+exports.watch = series(
+  compileSass,
+  compileJs,
+  browsersyncServer,
+  watchTask,
+);
+
 exports.sassc = compileSass;
+exports.jsc = compileJs;
